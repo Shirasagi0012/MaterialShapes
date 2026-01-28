b5171d2d7f0b6770eb70694be25aa32ae21a27db
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2026-01-28 20:39:39 +0800

    chore: enable cpm

---
+++ MaterialShapes.Gallery/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MaterialShapes.Gallery.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ MaterialShapes.Gallery/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MaterialShapes.Gallery.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
